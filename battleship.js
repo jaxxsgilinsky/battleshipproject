03fba8abcfad7bfcf0c24c9d41d03867685a8966
Jackie Guzman <jackieguzman3241@gmail.com>
2016-09-02 09:13:37 -0700

    Started to add text and numbers to the board

--- battleship.js
+++ battleship.js
@@ -2,6 +2,7 @@
 var rows = 10;
 var cols = 10;
 var squareSize = 50;
+var letterArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",];
 
 // gets the container element
 var gameBoardContainer = document.getElementById("gameboard");
@@ -33,7 +34,11 @@ for (i = 0; i < cols; i++) {
 		square.id = 's' + j + i;
 		square.className = "boardSquare";
 
-		// THIS IS WHERE YOU WILL ADD CODE FOR PART 1 TO ADD TEXT TO EACH SQUARE
+    // This is where your code for part 2 should go
+    var letterArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",];
+		square.textContent = letterArray[j] +=
+		square.textContent = i + 1;
+
 
 		// set each grid square's coordinates: multiples of the current row or column number
 		var topPosition = j * squareSize;
